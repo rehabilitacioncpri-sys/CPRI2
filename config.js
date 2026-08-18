f85967739dfb88c9727eaf282135d36311f805e1
rehabilitacioncpri-sys <rehabilitacioncpri@gmail.com>
2026-08-18 11:54:20 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.CPRI_CONFIG={
-    url:"https://gmukftrtnuxjaohvoyvw.supabase.co/rest/v1/",
+    url:"https://gmukftrtnuxjaohvoyvw.supabase.co",
     anonKey:"sb_publishable_ro67Rp_El4w2AAmjjUzu4g_AjdOvrFR"
 };
