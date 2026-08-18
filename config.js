0953793812237efc1dc18667e38edf67f4c6ca38
rehabilitacioncpri-sys <rehabilitacioncpri@gmail.com>
2026-08-18 11:49:51 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.CPRI_CONFIG={
     url:"https://gmukftrtnuxjaohvoyvw.supabase.co/rest/v1/",
-    anonKey:"sb_publishable_ro67Rp_El4w2AAmjjUzu4g_AjdOvrFR"
+    anonKey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdtdWtmdHJ0bnV4amFvaHZveXZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY5ODMxMzAsImV4cCI6MjEwMjU1OTEzMH0.ulKA0yOry_8HpVj-OYbyjwAJpyE16Gte3U7H1k0epfo"
 };
